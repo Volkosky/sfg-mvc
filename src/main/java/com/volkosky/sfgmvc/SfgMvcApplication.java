3fc7b4d01fe44bbfc2045c0889f24ad94d470a2e
Mateusz Wołek <volkosky@gmail.com>
2020-11-20 21:14:56 +0100

    init commit

---
+++ src/main/java/com/volkosky/sfgmvc/SfgMvcApplication.java
@@ -0,0 +1,13 @@
+package com.volkosky.sfgmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/volkosky/sfgmvc/SfgMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volkosky.sfgmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
